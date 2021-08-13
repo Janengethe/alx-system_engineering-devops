f926ab5d3180546595338d5c710c1f6a8ca8d15b
Janengethe <janengethew@gmail.com>
2021-08-13 11:16:44 +0000

    initial commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file in /tmp/holberton usinf puppet
+
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
