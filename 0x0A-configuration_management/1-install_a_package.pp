2f93e555cfe4a554c0f6e433a2168d39a5cc2706
Jane Ng'ethe <77954554+Janengethe@users.noreply.github.com>
2021-12-02 14:32:01 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# script to install puppet-lint using puppet
-
-package {'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# creates a manifest that kills 'killmenow'
-
-exec { 'pkill killmenow':
-  command => 'pkill -f killmenow'
-}
