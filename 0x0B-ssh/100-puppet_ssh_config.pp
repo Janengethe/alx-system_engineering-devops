1da6f330a2f46b857d66a7e1e8f49ca21c25b2ca
Janengethe <janengethew@gmail.com>
2022-01-03 22:18:29 -0800

    password congig using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
-#configures file to connect to server without password
+#configures to connect server without password
 
 file_line { 'PasswordAuthentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no'
+  line   => 'PasswordAuthentication no',
 }
-file_line { 'IdentityFile':
+file_line {'IdentityFile':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
