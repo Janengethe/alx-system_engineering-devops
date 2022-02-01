1e9cacf1cd69b98005e396651cfd3ae10a36bb45
Janengethe <janengethew@gmail.com>
2022-01-31 18:30:21 -0800

    to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module 0-gather_data_from_an_API
+Using "https://jsonplaceholder.typicode.com/"
+Returns information about his/her TODO list progress
+"""
+import csv
+import requests
+from sys import argv
+
+
+def gather_data_to_csv():
+    """Fetches data of employees and their todo tasks"""
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(users_url)
+    for i in users.json():
+        if i.get("id") == int(argv[1]):
+            USERNAME = i.get("username")
+            break
+    TASK_STATUS_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
+
+    """export to csv"""
+    filename = "{}.csv".format(argv[1])
+    with open(filename, "w") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for task in TASK_STATUS_TITLE:
+            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
+                             "TASK_COMPLETED_STATUS": task[0],
+                             "TASK_TITLE": task[1]})
+if __name__ == "__main__":
+    gather_data_to_csv()
