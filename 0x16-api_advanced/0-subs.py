615302339d1edc00196c40fc089533499b491d8e
Janengethe <janengethew@gmail.com>
2022-02-15 17:14:46 +0300

    user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns number of total subscribers"""
     url = ("https://api.reddit.com/r/{}/about".format(subreddit))
-    headers = {'User-Agent': 'Janengethe'}
+    headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
