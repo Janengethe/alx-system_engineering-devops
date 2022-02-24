f159c3c9b8f7f4bb0b194c8c8a9ca040767e70a6
Janengethe <janengethew@gmail.com>
2022-02-24 13:09:34 +0300

    puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,8 +4,8 @@ exec { 'nginx requests limit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
-#Restart NginX
 
+#Restart NginX
 exec { 'restart nginx':
   command => 'nginx restart',
   path    => '/etc/init.d/'
