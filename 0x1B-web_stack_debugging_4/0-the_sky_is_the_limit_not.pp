11a0220dd0b294196aa356f196ad950e583ee7ba
Janengethe <janengethew@gmail.com>
2022-02-25 13:15:47 +0300

    limit set

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # fix nginx to accept and serve more requests
 
 exec { 'nginx requests limit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "s/15/1000/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
